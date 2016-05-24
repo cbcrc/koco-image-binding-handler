1118812d49ff770db784a81ce23e697584d5a36b
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:21:08 -0400

    update to koco2

--- src/image-binding-handler.js
+++ src/image-binding-handler.js
@@ -1,60 +1,60 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(['knockout', 'jquery', 'image-utilities',
-    'mapping-utilities'],
-    function(ko, $, imageUtilities, mappingUtilities) {
-        'use strict';
+import ko from 'knockout';
+import $ from 'jquery';
+import imageUtilities from 'image-utilities';
+import mappingUtilities from 'mapping-utilities';
 
-        ko.bindingHandlers.image = {
-            update: function(element, valueAccessor, allBindingsAccessor) {
-                var allBindings = allBindingsAccessor(),
-                    conceptualImage = mappingUtilities.toJS(valueAccessor()),
-                    $element = $(element),
-                    options = $.extend({
-                        concreteImageOptions: imageUtilities.defaultConcreteImageOptions,
-                        displayMaxWidth: 480,
-                        displayMaxHeight: 270
-                    }, allBindings.imageOptions || {});
 
-                var imageUrl = getImageUrl(conceptualImage, options);
+ko.bindingHandlers.image = {
+    update: function(element, valueAccessor, allBindingsAccessor) {
+        var allBindings = allBindingsAccessor(),
+            conceptualImage = mappingUtilities.toJS(valueAccessor()),
+            $element = $(element),
+            options = $.extend({
+                concreteImageOptions: imageUtilities.defaultConcreteImageOptions,
+                displayMaxWidth: 480,
+                displayMaxHeight: 270
+            }, allBindings.imageOptions || {});
 
-                if (imageUrl) {
-                    if (options.displayMaxWidth && options.displayMaxHeight) {
-                        $element.css('max-width', options.displayMaxWidth);
-                        $element.css('max-height', options.displayMaxHeight);
+        var imageUrl = getImageUrl(conceptualImage, options);
 
-                        if (isPictoImage(conceptualImage)) {
-                            imageUrl = updateImageUrlWithMaxDimensions(imageUrl, options.displayMaxWidth, options.displayMaxHeight);
-                        }
-                    }
+        if (imageUrl) {
+            if (options.displayMaxWidth && options.displayMaxHeight) {
+                $element.css('max-width', options.displayMaxWidth);
+                $element.css('max-height', options.displayMaxHeight);
 
-                    $element.attr('src', imageUrl);
-                    $element.show();
-                } else {
-                    $element.hide();
-                    $element.removeAttr('src');
-                }
-            }
-        };
-
-        function getImageUrl(conceptualImage, options) {
-            if (conceptualImage && conceptualImage.concreteImages) {
-                var concreteImage = imageUtilities.getConcreteImage(conceptualImage, options.concreteImageOptions);
-                if (concreteImage && concreteImage.mediaLink.href) {
-                    return concreteImage.mediaLink.href;
+                if (isPictoImage(conceptualImage)) {
+                    imageUrl = updateImageUrlWithMaxDimensions(imageUrl, options.displayMaxWidth, options.displayMaxHeight);
                 }
             }
 
-            return options.default;
+            $element.attr('src', imageUrl);
+            $element.show();
+        } else {
+            $element.hide();
+            $element.removeAttr('src');
         }
+    }
+};
 
-        function isPictoImage(conceptualImage) {
-            return Boolean(conceptualImage && conceptualImage.contentType && conceptualImage.contentType.id === 19);
+function getImageUrl(conceptualImage, options) {
+    if (conceptualImage && conceptualImage.concreteImages) {
+        var concreteImage = imageUtilities.getConcreteImage(conceptualImage, options.concreteImageOptions);
+        if (concreteImage && concreteImage.mediaLink.href) {
+            return concreteImage.mediaLink.href;
         }
+    }
 
-        function updateImageUrlWithMaxDimensions(imageUrl, maxWidth, maxHeight) {
-            var fitTransformation = '/w_' + maxWidth + ',h_' + maxHeight + ',c_fit';
-            return imageUrl.replace('/v1/', fitTransformation + '/v1/');
-        }
-});
+    return options.default;
+}
+
+function isPictoImage(conceptualImage) {
+    return Boolean(conceptualImage && conceptualImage.contentType && conceptualImage.contentType.id === 19);
+}
+
+function updateImageUrlWithMaxDimensions(imageUrl, maxWidth, maxHeight) {
+    var fitTransformation = '/w_' + maxWidth + ',h_' + maxHeight + ',c_fit';
+    return imageUrl.replace('/v1/', fitTransformation + '/v1/');
+}
