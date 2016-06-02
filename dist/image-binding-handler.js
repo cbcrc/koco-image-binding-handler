04533145aa71dc44a7498bf5bbb501b4d2722b1b
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-06-02 16:30:52 -0400

    koco2...

---
+++ dist/image-binding-handler.js
@@ -0,0 +1,84 @@
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(['knockout', 'jquery', 'koco-image-utilities', 'koco-mapping-utilities'], factory);
+    } else if (typeof exports !== "undefined") {
+        factory(require('knockout'), require('jquery'), require('koco-image-utilities'), require('koco-mapping-utilities'));
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(global.knockout, global.jquery, global.kocoImageUtilities, global.kocoMappingUtilities);
+        global.imageBindingHandler = mod.exports;
+    }
+})(this, function (_knockout, _jquery, _kocoImageUtilities, _kocoMappingUtilities) {
+    'use strict';
+
+    var _knockout2 = _interopRequireDefault(_knockout);
+
+    var _jquery2 = _interopRequireDefault(_jquery);
+
+    var _kocoImageUtilities2 = _interopRequireDefault(_kocoImageUtilities);
+
+    var _kocoMappingUtilities2 = _interopRequireDefault(_kocoMappingUtilities);
+
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+
+    // Copyright (c) CBC/Radio-Canada. All rights reserved.
+    // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+    _knockout2.default.bindingHandlers.image = {
+        update: function update(element, valueAccessor, allBindingsAccessor) {
+            var allBindings = allBindingsAccessor(),
+                conceptualImage = _kocoMappingUtilities2.default.toJS(valueAccessor()),
+                $element = (0, _jquery2.default)(element),
+                options = _jquery2.default.extend({
+                concreteImageOptions: _kocoImageUtilities2.default.defaultConcreteImageOptions,
+                displayMaxWidth: 480,
+                displayMaxHeight: 270
+            }, allBindings.imageOptions || {});
+
+            var imageUrl = getImageUrl(conceptualImage, options);
+
+            if (imageUrl) {
+                if (options.displayMaxWidth && options.displayMaxHeight) {
+                    $element.css('max-width', options.displayMaxWidth);
+                    $element.css('max-height', options.displayMaxHeight);
+
+                    if (isPictoImage(conceptualImage)) {
+                        imageUrl = updateImageUrlWithMaxDimensions(imageUrl, options.displayMaxWidth, options.displayMaxHeight);
+                    }
+                }
+
+                $element.attr('src', imageUrl);
+                $element.show();
+            } else {
+                $element.hide();
+                $element.removeAttr('src');
+            }
+        }
+    };
+
+    function getImageUrl(conceptualImage, options) {
+        if (conceptualImage && conceptualImage.concreteImages) {
+            var concreteImage = _kocoImageUtilities2.default.getConcreteImage(conceptualImage, options.concreteImageOptions);
+            if (concreteImage && concreteImage.mediaLink.href) {
+                return concreteImage.mediaLink.href;
+            }
+        }
+
+        return options.default;
+    }
+
+    function isPictoImage(conceptualImage) {
+        return Boolean(conceptualImage && conceptualImage.contentType && conceptualImage.contentType.id === 19);
+    }
+
+    function updateImageUrlWithMaxDimensions(imageUrl, maxWidth, maxHeight) {
+        var fitTransformation = '/w_' + maxWidth + ',h_' + maxHeight + ',c_fit';
+        return imageUrl.replace('/v1/', fitTransformation + '/v1/');
+    }
+});
