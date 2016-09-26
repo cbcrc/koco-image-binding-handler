d44200fd4070255c2f0beb697e5085dda02868ad
Laurent le Beau-Martin <laurent.lebeau-martin@radio-canada.ca>
2016-09-26 13:43:59 -0400

    Update image when options change

--- dist/image-binding-handler.js
+++ dist/image-binding-handler.js
@@ -1,84 +1,98 @@
 (function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(['knockout', 'jquery', 'koco-image-utilities', 'koco-mapping-utilities'], factory);
-    } else if (typeof exports !== "undefined") {
-        factory(require('knockout'), require('jquery'), require('koco-image-utilities'), require('koco-mapping-utilities'));
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory(global.knockout, global.jquery, global.kocoImageUtilities, global.kocoMappingUtilities);
-        global.imageBindingHandler = mod.exports;
-    }
-})(this, function (_knockout, _jquery, _kocoImageUtilities, _kocoMappingUtilities) {
-    'use strict';
-
-    var _knockout2 = _interopRequireDefault(_knockout);
-
-    var _jquery2 = _interopRequireDefault(_jquery);
-
-    var _kocoImageUtilities2 = _interopRequireDefault(_kocoImageUtilities);
-
-    var _kocoMappingUtilities2 = _interopRequireDefault(_kocoMappingUtilities);
-
-    function _interopRequireDefault(obj) {
-        return obj && obj.__esModule ? obj : {
-            default: obj
-        };
-    }
-
-    // Copyright (c) CBC/Radio-Canada. All rights reserved.
-    // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-    _knockout2.default.bindingHandlers.image = {
-        update: function update(element, valueAccessor, allBindingsAccessor) {
-            var allBindings = allBindingsAccessor(),
-                conceptualImage = _kocoMappingUtilities2.default.toJS(valueAccessor()),
-                $element = (0, _jquery2.default)(element),
-                options = _jquery2.default.extend({
-                concreteImageOptions: _kocoImageUtilities2.default.defaultConcreteImageOptions,
-                displayMaxWidth: 480,
-                displayMaxHeight: 270
-            }, allBindings.imageOptions || {});
-
-            var imageUrl = getImageUrl(conceptualImage, options);
-
-            if (imageUrl) {
-                if (options.displayMaxWidth && options.displayMaxHeight) {
-                    $element.css('max-width', options.displayMaxWidth);
-                    $element.css('max-height', options.displayMaxHeight);
-
-                    if (isPictoImage(conceptualImage)) {
-                        imageUrl = updateImageUrlWithMaxDimensions(imageUrl, options.displayMaxWidth, options.displayMaxHeight);
-                    }
-                }
-
-                $element.attr('src', imageUrl);
-                $element.show();
-            } else {
-                $element.hide();
-                $element.removeAttr('src');
-            }
-        }
+  if (typeof define === "function" && define.amd) {
+    define(['knockout', 'jquery', 'koco-image-utilities'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(require('knockout'), require('jquery'), require('koco-image-utilities'));
+  } else {
+    var mod = {
+      exports: {}
     };
+    factory(global.knockout, global.jquery, global.kocoImageUtilities);
+    global.imageBindingHandler = mod.exports;
+  }
+})(this, function (_knockout, _jquery, _kocoImageUtilities) {
+  'use strict';
 
-    function getImageUrl(conceptualImage, options) {
-        if (conceptualImage && conceptualImage.concreteImages) {
-            var concreteImage = _kocoImageUtilities2.default.getConcreteImage(conceptualImage, options.concreteImageOptions);
-            if (concreteImage && concreteImage.mediaLink.href) {
-                return concreteImage.mediaLink.href;
-            }
+  var _knockout2 = _interopRequireDefault(_knockout);
+
+  var _jquery2 = _interopRequireDefault(_jquery);
+
+  var _kocoImageUtilities2 = _interopRequireDefault(_kocoImageUtilities);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  var defaultImageOptions = {
+    concreteImageOptions: _kocoImageUtilities2.default.defaultConcreteImageOptions,
+    displayMaxWidth: 480,
+    displayMaxHeight: 270
+  }; // Copyright (c) CBC/Radio-Canada. All rights reserved.
+  // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+  function getImageUrl(conceptualImage, options) {
+    if (conceptualImage && conceptualImage.concreteImages) {
+      var concreteImage = _kocoImageUtilities2.default.getConcreteImage(conceptualImage, options.concreteImageOptions);
+      if (concreteImage && concreteImage.mediaLink.href) {
+        return concreteImage.mediaLink.href;
+      }
+    }
+
+    return options.default;
+  }
+
+  function isPictoImage(conceptualImage) {
+    return Boolean(conceptualImage && conceptualImage.contentType && conceptualImage.contentType.id === 19);
+  }
+
+  function updateImageUrlWithMaxDimensions(imageUrl, maxWidth, maxHeight) {
+    var fitTransformation = '/w_' + maxWidth + ',h_' + maxHeight + ',c_limit';
+    return imageUrl.replace('/v1/', fitTransformation + '/v1/');
+  }
+
+  function setImage($element, conceptualImage, imageOptions) {
+    var options = _jquery2.default.extend({}, defaultImageOptions, _knockout2.default.toJS(imageOptions));
+    var imageUrl = getImageUrl(conceptualImage, options);
+
+    if (imageUrl) {
+      if (options.displayMaxWidth && options.displayMaxHeight) {
+        $element.css('max-width', options.displayMaxWidth);
+        $element.css('max-height', options.displayMaxHeight);
+
+        if (isPictoImage(conceptualImage)) {
+          imageUrl = updateImageUrlWithMaxDimensions(imageUrl, options.displayMaxWidth, options.displayMaxHeight);
         }
+      }
 
-        return options.default;
+      $element.attr('src', imageUrl);
+      $element.show();
+    } else {
+      $element.hide();
+      $element.removeAttr('src');
     }
+  }
 
-    function isPictoImage(conceptualImage) {
-        return Boolean(conceptualImage && conceptualImage.contentType && conceptualImage.contentType.id === 19);
-    }
+  _knockout2.default.bindingHandlers.image = {
+    update: function update(element, valueAccessor, allBindingsAccessor) {
+      var $element = (0, _jquery2.default)(element);
+      var conceptualImage = _knockout2.default.toJS(valueAccessor());
+      var imageOptionsBinding = allBindingsAccessor.get('imageOptions');
+      var imageOptions = _knockout2.default.toJS(imageOptionsBinding);
 
-    function updateImageUrlWithMaxDimensions(imageUrl, maxWidth, maxHeight) {
-        var fitTransformation = '/w_' + maxWidth + ',h_' + maxHeight + ',c_fit';
-        return imageUrl.replace('/v1/', fitTransformation + '/v1/');
+      setImage($element, conceptualImage, imageOptions);
+
+      if (_knockout2.default.isObservable(imageOptionsBinding)) {
+        (function () {
+          var subscription = imageOptionsBinding.subscribe(function (newOptions) {
+            return setImage($element, conceptualImage, newOptions);
+          });
+          _knockout2.default.utils.domNodeDisposal.addDisposeCallback(element, function () {
+            subscription.dispose();
+          });
+        })();
+      }
     }
+  };
 });
--- src/image-binding-handler.js
+++ src/image-binding-handler.js
@@ -4,57 +4,67 @@
 import ko from 'knockout';
 import $ from 'jquery';
 import imageUtilities from 'koco-image-utilities';
-import mappingUtilities from 'koco-mapping-utilities';
 
-
-ko.bindingHandlers.image = {
-    update: function(element, valueAccessor, allBindingsAccessor) {
-        var allBindings = allBindingsAccessor(),
-            conceptualImage = mappingUtilities.toJS(valueAccessor()),
-            $element = $(element),
-            options = $.extend({
-                concreteImageOptions: imageUtilities.defaultConcreteImageOptions,
-                displayMaxWidth: 480,
-                displayMaxHeight: 270
-            }, allBindings.imageOptions || {});
-
-        var imageUrl = getImageUrl(conceptualImage, options);
-
-        if (imageUrl) {
-            if (options.displayMaxWidth && options.displayMaxHeight) {
-                $element.css('max-width', options.displayMaxWidth);
-                $element.css('max-height', options.displayMaxHeight);
-
-                if (isPictoImage(conceptualImage)) {
-                    imageUrl = updateImageUrlWithMaxDimensions(imageUrl, options.displayMaxWidth, options.displayMaxHeight);
-                }
-            }
-
-            $element.attr('src', imageUrl);
-            $element.show();
-        } else {
-            $element.hide();
-            $element.removeAttr('src');
-        }
-    }
+const defaultImageOptions = {
+  concreteImageOptions: imageUtilities.defaultConcreteImageOptions,
+  displayMaxWidth: 480,
+  displayMaxHeight: 270
 };
 
 function getImageUrl(conceptualImage, options) {
-    if (conceptualImage && conceptualImage.concreteImages) {
-        var concreteImage = imageUtilities.getConcreteImage(conceptualImage, options.concreteImageOptions);
-        if (concreteImage && concreteImage.mediaLink.href) {
-            return concreteImage.mediaLink.href;
-        }
+  if (conceptualImage && conceptualImage.concreteImages) {
+    const concreteImage = imageUtilities.getConcreteImage(conceptualImage, options.concreteImageOptions);
+    if (concreteImage && concreteImage.mediaLink.href) {
+      return concreteImage.mediaLink.href;
     }
+  }
 
-    return options.default;
+  return options.default;
 }
 
 function isPictoImage(conceptualImage) {
-    return Boolean(conceptualImage && conceptualImage.contentType && conceptualImage.contentType.id === 19);
+  return Boolean(conceptualImage && conceptualImage.contentType && conceptualImage.contentType.id === 19);
 }
 
 function updateImageUrlWithMaxDimensions(imageUrl, maxWidth, maxHeight) {
-    var fitTransformation = '/w_' + maxWidth + ',h_' + maxHeight + ',c_fit';
-    return imageUrl.replace('/v1/', fitTransformation + '/v1/');
+  const fitTransformation = `/w_${maxWidth},h_${maxHeight},c_limit`;
+  return imageUrl.replace('/v1/', `${fitTransformation}/v1/`);
 }
+
+function setImage($element, conceptualImage, imageOptions) {
+  const options = $.extend({}, defaultImageOptions, ko.toJS(imageOptions));
+  let imageUrl = getImageUrl(conceptualImage, options);
+
+  if (imageUrl) {
+    if (options.displayMaxWidth && options.displayMaxHeight) {
+      $element.css('max-width', options.displayMaxWidth);
+      $element.css('max-height', options.displayMaxHeight);
+
+      if (isPictoImage(conceptualImage)) {
+        imageUrl = updateImageUrlWithMaxDimensions(imageUrl, options.displayMaxWidth, options.displayMaxHeight);
+      }
+    }
+
+    $element.attr('src', imageUrl);
+    $element.show();
+  } else {
+    $element.hide();
+    $element.removeAttr('src');
+  }
+}
+
+ko.bindingHandlers.image = {
+  update(element, valueAccessor, allBindingsAccessor) {
+    const $element = $(element);
+    const conceptualImage = ko.toJS(valueAccessor());
+    const imageOptionsBinding = allBindingsAccessor.get('imageOptions');
+    const imageOptions = ko.toJS(imageOptionsBinding);
+
+    setImage($element, conceptualImage, imageOptions);
+
+    if (ko.isObservable(imageOptionsBinding)) {
+      const subscription = imageOptionsBinding.subscribe(newOptions => setImage($element, conceptualImage, newOptions));
+      ko.utils.domNodeDisposal.addDisposeCallback(element, () => { subscription.dispose(); });
+    }
+  }
+};
